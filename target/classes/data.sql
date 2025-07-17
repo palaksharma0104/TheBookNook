288f92a87edbebca3c37449e464ddb7b5ad29963
palaksharma0104 <palak16221@gmail.com>
2025-07-17 09:46:16 +0530

    commit

---
+++ target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO books (title, author, genre, description) VALUES
+('Kafka on the Shore', 'Haruki Murakami', 'Magical Realism', 'A surreal journey of a teenage boy and an aging man whose lives intertwine mysteriously.'),
+('The Secret History', 'Donna Tartt', 'Dark Academia', 'A group of classics students at an elite college descend into obsession and murder.'),
+('Pride and Prejudice', 'Jane Austen', 'Romance', 'The story of Elizabeth Bennet as she navigates issues of manners, upbringing, and love in 19th century England.'),
+('Circe', 'Madeline Miller', 'Mythological Fiction', 'A reimagining of the life of Circe, the enchantress from Homer’s Odyssey, who dares to live on her own terms.'),
+('1984', 'George Orwell', 'Dystopian', 'A chilling portrayal of a totalitarian regime that uses surveillance and propaganda to oppress its people.'),
+('If We Were Villains', 'M.L. Rio', 'Mystery/Thriller', 'A group of Shakespearean actors find their real lives echoing the tragedies they perform as one of them ends up dead.');
